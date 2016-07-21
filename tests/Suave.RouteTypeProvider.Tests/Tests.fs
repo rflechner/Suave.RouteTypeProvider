32c3a381c0ad697a6632a0e0aa7828861588a3eb
Flechner Romain <rflechner@romcyber.com>
2016-07-21 11:05:40 +0200

    init

---
+++ tests/Suave.RouteTypeProvider.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module Suave.RouteTypeProvider.Tests
+
+open Suave.RouteTypeProvider
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
