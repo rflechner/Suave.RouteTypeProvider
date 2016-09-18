21b662237199d2b58adc93fcb4a8fd05d88f1243
Flechner Romain <rflechner@romcyber.com>
2016-09-18 18:30:02 +0200

    fix description

--- src/Suave.RouteTypeProvider/AssemblyInfo.fs
+++ src/Suave.RouteTypeProvider/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Suave.RouteTypeProvider")>]
 [<assembly: AssemblyProductAttribute("Suave.RouteTypeProvider")>]
 [<assembly: AssemblyDescriptionAttribute("A Type Provider helping to create strongly typed routes like WebApi format")>]
-[<assembly: AssemblyVersionAttribute("0.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+[<assembly: AssemblyVersionAttribute("0.0.3")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.1"
-    let [<Literal>] InformationalVersion = "0.0.1"
+    let [<Literal>] Version = "0.0.3"
+    let [<Literal>] InformationalVersion = "0.0.3"
